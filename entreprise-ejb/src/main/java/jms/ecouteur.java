d940272f1ff5b4e54a4fff6188d5b7ef3fd0dac2
Chambriard <alban.chambriard@gmail.com>
2019-12-02 19:14:18 +0100

    init

---
+++ entreprise-ejb/src/main/java/jms/ecouteur.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jms;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+/**
+ *
+ * @author alban
+ */
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "commercialEntreprise")
+    ,
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class ecouteur implements MessageListener {
+    
+    public ecouteur() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        try {
+            System.out.println("entreprise : " +message.getBody(String.class));
+        } catch (JMSException ex) {
+            Logger.getLogger(ecouteur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
---
+++ entreprise-ejb/src/main/java/metier/gestionEntreprise.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author alban
+ */
+@Stateless
+public class gestionEntreprise implements gestionEntrepriseLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
